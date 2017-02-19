09c8dc3a3c7b21b2a4b35bc1978a06a0e7b0f734
dison  ruan <disonruan@disons-Air.fios-router.home>
2017-02-19 16:50:38 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Quackers"
+  end
+
+  get '/hometown' do
+    "My hometown is duck pen"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is again"
+  end
+
 end
